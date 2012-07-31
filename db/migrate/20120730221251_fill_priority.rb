b9a57d7a3acd5ea7fcf5417a04cb8422cd002adc
Alexey <palexvs@gmail.com>
2012-07-31 18:00:55 +0300

    fix migration file

--- db/migrate/20120730221251_fill_priority.rb
+++ db/migrate/20120730221251_fill_priority.rb
@@ -1,15 +1,10 @@
 class FillPriority < ActiveRecord::Migration
-  def up
+  def change
     Task.all.each do |task|
       task.update_attributes(priority: task.id) 
     end
-  end
 
-  def change
     add_index :tasks, [:project_id, :priority]
     add_index :tasks, [:project_id, :id]
-  end
-
-  def down
   end
 end
