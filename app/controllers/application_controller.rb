611d1d051371cfb5cf965e13d9bbcfa36b080bfe
Alexey <palexvs@gmail.com>
2012-08-15 17:32:00 +0300

    try handle not exists routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
+ rescue_from ActiveRecord::RecordNotFound, :with => :rescue404
+ rescue_from ActionController::RoutingError, :with => :rescue404
+
+  def rescue404
+    redirect_to root_path, :notice => "The page you were looking for doesn't exist."
+  end
 
   private
 
